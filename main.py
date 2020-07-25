98bc6becbc93b8a3fcb16e19a044e036c70abd88
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-25 23:56:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from tkinter import *
 import calendar
 
+def show():
+    year = int(spin2.get())
+    mont = int(spin1.get())
+    cal = calendar.month(year,mont)
+    screen.delete(0.0,END)
+    screen.insert(INSERT,cal)
+
 root = Tk()
 root.title('Calender')
-root.geometry('500x500')
-
+root.geometry('300x350')
+root.configure(background='olive')
 Label(root,text='Calender',font='Arial 20 bold').pack(pady=10)
 frm1 = Frame(root)
 Label(frm1,text='Month',font='arial 12 bold').pack(side=LEFT)
@@ -14,7 +21,8 @@ Label(frm1,text='Year',font='arial 12 bold').pack(padx=5,side=LEFT)
 spin2 = Spinbox(frm1,from_=1947,to_=2030,width=5,font='arial 12')
 spin2.pack(side=LEFT,padx=5)
 frm1.pack(pady=10)
-Button(text='Show',font='arial 10 bold', command=None).pack(pady=10)
-screen = Text(root, width=30,height=10).pack()
+Button(text='Show',font='arial 10 bold', command=show).pack(pady=10)
+screen = Text(root, width=30,height=10)
+screen.pack()
 
 root.mainloop()
